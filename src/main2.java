5c01e220f2bb1afae08192f3fa226e200407c528
KawhiYy <45116594+KawhiYy@users.noreply.github.com>
2018-11-29 23:45:31 +0800

    Update main2.java

--- src/main2.java
+++ src/main2.java
@@ -1,9 +1,9 @@
 public class main2 {
 	public static void main(String[] args) {
 		CustomList<String> aa = new CustomList(10);
-		aa.add("������");
+		aa.add("eee");
 		aa.add("hhh");
-		aa.set(1, "����������");
+		aa.set(1, "yyy");
 		aa.print();
 	}
 }
